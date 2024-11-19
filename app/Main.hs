e429de6b76c60bf1a78c3bd8b5ccbc28be98485a
Maksen <maksen.mofredj@gmail.com>
2024-11-19 18:21:52 +0100

    [ADD] safe file getting/reading implemented

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,18 @@
 module Main (main) where
 
 import Lib
+import System.Exit
+import System.Environment
+
+getFileName :: [String] -> Maybe String
+getFileName [a] = Just a
+getFileName _ = Nothing
 
 main :: IO ()
-main = someFunc
+main = do
+    args <- getArgs
+    filename <- case getFileName args of
+                Nothing -> exitWith(ExitFailure 84)
+                Just filename -> pure filename
+    file <- readFile filename
+    putStrLn file
