183234b47d67d2597d6fccb06cf16486799e690c
PERRIOLAT Nils <nils.perriolat@epitech.eu>
2024-11-25 08:44:35 -0500

    [ADD] missing pattern

--- app/Main.hs
+++ app/Main.hs
@@ -1,18 +1,21 @@
 module Main (main) where
 
-import Lib
-import System.Exit
-import System.Environment
+import SExpression
+--import System.Exit
+--import System.Environment
 
-getFileName :: [String] -> Maybe String
-getFileName [a] = Just a
-getFileName _ = Nothing
+--getFileName :: [String] -> Maybe String
+--getFileName [a] = Just a
+--getFileName _ = Nothing
+
+--main :: IO ()
+--main = do
+--    args <- getArgs
+--    filename <- case getFileName args of
+--                Nothing -> exitWith(ExitFailure 84)
+--                Just filename -> pure filename
+--    file <- readFile filename
+--    putStrLn file
 
 main :: IO ()
-main = do
-    args <- getArgs
-    filename <- case getFileName args of
-                Nothing -> exitWith(ExitFailure 84)
-                Just filename -> pure filename
-    file <- readFile filename
-    putStrLn file
+main = print $ sexprToAST $ ex1
