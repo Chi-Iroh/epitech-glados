f00785efc06540a7d86a8076ba8335867505fdf1
PERRIOLAT Nils <nils.perriolat@epitech.eu>
2024-11-26 06:32:15 -0500

    [ADD] link main to parser

--- app/Main.hs
+++ app/Main.hs
@@ -2,24 +2,21 @@ module Main (main) where
 
 import SExpression
 import System.Exit
---import System.Environment
+import System.Environment
 
---getFileName :: [String] -> Maybe String
---getFileName [a] = Just a
---getFileName _ = Nothing
-
---main :: IO ()
---main = do
---    args <- getArgs
---    filename <- case getFileName args of
---                Nothing -> exitWith(ExitFailure 84)
---                Just filename -> pure filename
---    file <- readFile filename
---    putStrLn file
+getFileName :: [String] -> Maybe String
+getFileName [a] = Just a
+getFileName _ = Nothing
 
 checkError :: Maybe SExpr -> IO ()
 checkError (Just e) = print e
 checkError _ = exitWith(ExitFailure 84)
 
 main :: IO ()
-main = checkError $ parseStringToSExpr "if (x < (y + 1)) True False"
+main = do
+    args <- getArgs
+    filename <- case getFileName args of
+                Nothing -> exitWith(ExitFailure 84)
+                Just filename -> pure filename
+    file <- readFile filename
+    checkError $ parseStringToSExpr file
