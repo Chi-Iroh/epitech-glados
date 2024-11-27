42c4545fecfe579379b38c4453a4930a7570ab34
Thomas Sayen <thomas.sayen@epitech.eu>
2024-11-27 17:05:10 +0100

    [RM] Bootstrap useless functions

--- bootstrap/Main.hs
+++ bootstrap/Main.hs
@@ -13,46 +13,6 @@ debug2 :: Show a => String -> a -> b -> b
 debug2 msg a b = trace (msg ++ (show a)) b
 
 data SExpr = Number Int | Symbol String | List [SExpr] | Define String SExpr | Lambda SExpr SExpr deriving Show
-
-ex1 :: SExpr
-ex1 = List [
-    List [Symbol "x", Number 5],
-    Symbol "x",
-    List [Symbol "if", List [Symbol ">", Symbol "x", Number 4], Number 1, Number 0],
-    List [Symbol "y", List [Symbol "+", Number 5, Symbol "x"]]]
-
-getSymbol :: SExpr -> Maybe String
-getSymbol (Symbol symbol) = Just symbol
-getSymbol _ = Nothing
-
-getInteger :: SExpr -> Maybe Int
-getInteger (Number n) = Just n
-getInteger _ = Nothing
-
-getList :: SExpr -> Maybe [SExpr]
-getList (List exprs) = Just exprs
-getList _ = Nothing
-
-concatStrings :: Maybe String -> Maybe String -> Maybe String
-concatStrings Nothing Nothing = Nothing
-concatStrings Nothing other = other
-concatStrings first Nothing = first
-concatStrings (Just first) (Just other) = Just $ concat [first, other]
-
-joinStrings :: String -> [Maybe String] -> Maybe String
-joinStrings sep = foldr (\a b -> concatStrings a (concatStrings (b *> Just sep) b)) Nothing
-
-printTree :: SExpr -> Maybe String
-printTree (Number n) = Just $ concat ["a Number ", show n]
-printTree (Symbol symbol) = Just $ concat ["a Symbol ", symbol]
-printTree (List exprs) = concatStrings (Just "a List with ") (joinStrings " followed by " (map printTree exprs))
-
-mainSExpr :: IO ()
-mainSExpr = print $ printTree $ List [Symbol "define", Symbol "y", List [Symbol "*", Symbol "x", Number 5]]
-
-fromSymbol :: SExpr -> String
-fromSymbol (Symbol s) = s
-
 data AST = ASTDefine String AST | ASTSymbol String | ASTNumber Int | ASTBoolean Bool | ASTCall String [AST] deriving Show
 
 sexprToAST :: SExpr -> Maybe AST
