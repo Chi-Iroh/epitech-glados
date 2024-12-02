16185cdaa4fe5f3859099faea020e37c13c2c6ac
PERRIOLAT Nils <nils.perriolat@epitech.eu>
2024-12-02 10:03:21 -0500

    [FIX] test of unit tests for parser

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,4 +2,4 @@ import Test.HUnit
 import TestParser
 
 main :: IO Counts
-main = do _ <- runTestTT tests
+main = runTestTT tests
--- test/TestParser.hs
+++ test/TestParser.hs
@@ -6,10 +6,10 @@ import SExpression
 import Parser
 
 test1 :: Test
-test1 = TestCase (assertEqual "parse '0'" (Value $ SNumber 0) (parse "0"))
+test1 = TestCase (assertEqual "parse '0'" (Value $ SList [(SNumber 0)]) (parse "0"))
 
 test2 :: Test
-test2 = TestCase (assertEqual "parse ')'" (Error "") (parse ")"))
+test2 = TestCase (assertEqual "parse ')'" (Error "GLaDOS: SyntaxError: unexpected ')' while parsing") (parse ")"))
 
 tests :: Test
 tests = TestList [TestLabel "test1" test1, TestLabel "test2" test2]
