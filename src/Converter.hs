6dd58ef2dba04fc4a59c1abbf25597e02eb2d859
Swan <swan.sallet@epitech.eu>
2024-12-02 17:15:15 +0100

    [ADD] Totally (supposed) working sexprToAST (1 exception) + [EDIT] .scm file

--- src/Converter.hs
+++ src/Converter.hs
@@ -13,30 +13,48 @@ sexprSListHandling [SNumber a] = Just (ASTNumber a)
 sexprSListHandling [SSymbol "#t"] = Just (ASTBoolean True)
 sexprSListHandling [SSymbol "#f"] = Just (ASTBoolean True)
 sexprSListHandling [SSymbol a] = Just (ASTSymbol a)
--- sexprSListHandling [SList a] = Just (ASTNumber 23) -- i don't want this
+sexprSListHandling (SList (SSymbol "lambda":b:c):rest)
+        | null c = Nothing
+        | otherwise = case sexprToAST [b] of
+            Just parameter -> case sexprToAST c of
+                Just [expression] -> case sexprToAST rest of
+                    Just rest -> Just (ASTCall (LambdaCall parameter expression) rest)
+                    Nothing -> Just (ASTCall (LambdaCall parameter expression) [])          -- if after lambdacall there is nothing
+                Nothing -> Nothing                                                          -- lambda must have expression
+            Nothing -> case sexprToAST c of
+                Just [expression] -> case sexprToAST rest of
+                    Just rest -> Just (ASTCall (LambdaCall [] expression) rest)
+                    Nothing -> Just (ASTCall (LambdaCall [] expression) [])                 -- if after lambdacall there is nothing
+                Nothing -> Nothing                                                          -- lambda must have expression
 
 sexprSListHandling (SSymbol "define":b:c)
-        | null [b] = Nothing                                            -- define must have a name
-        | null c = Nothing                                              -- defne must have a value
+        | null [b] = Nothing                                                                -- define must have a name
+        | null c = Nothing                                                                  -- defne must have a value
         | otherwise = case getSymbol b of
             Just symbol -> case sexprToAST c of
                 Just [result] -> Just (ASTDefine symbol result)
-                Nothing -> Nothing                                      -- cannot define variable without value in scheme
-            Nothing -> Nothing                                          -- if symbol is not a valid Ssymbol (should not happend)
+                Nothing -> Nothing                                                          -- cannot define variable without value in scheme
+            Nothing -> case sexprToAST c of                                                 -- if no symbol
+                Just [result] -> Just (ASTDefine "" result)
+                Nothing -> Nothing                                                          -- cannot define variable without value in scheme
 
 sexprSListHandling (SSymbol "lambda":b:c)
-        | null c = Nothing                                              -- lambda must have som expression (at least 1) but can have no parameter -- handle this case beacause currently not
+        | null c = Nothing                                                                  -- lambda must have som expression (at least 1) but can have no parameter
         | otherwise = case sexprToAST [b] of
             Just parameter -> case sexprToAST c of
                 Just [expression] -> Just (ASTLambda parameter expression)
-                Nothing -> Nothing
-            Nothing -> Nothing
+                Nothing -> Nothing                                                          -- lambda must have som expression (at least 1) but can have no parameter
+            Nothing -> case sexprToAST c of
+                Just [expression] -> Just (ASTLambda [] expression)
+                Nothing -> Nothing                                                          -- lambda must have som expression (at least 1) but can have no parameter
 
 sexprSListHandling (SSymbol a:b) = case getSymbol (SSymbol a) of
         Just symbol -> case sexprToAST b of
             Just result -> Just (ASTCall (FunctionCall symbol) result)
 
-sexprSListHandling _ = Just (ASTNumber 5) --debug
+sexprSListHandling (a:b) = Nothing -- case de merde que je veux pas
+
+-- sexprSListHandling _ = Just (ASTNumber 5) --debug
 
 
 sexprToAST :: [SExpr] -> Maybe [AST]
@@ -44,33 +62,15 @@ sexprToAST [] = Just []
 sexprToAST (SList elements:rest) = case sexprSListHandling elements of
     Just result -> case sexprToAST rest of
         Just restAST -> Just (result : restAST)
-        Nothing -> Nothing                                              -- Error in processing the rest
-    Nothing -> Nothing                                                  -- Error in processing the current list
+        Nothing -> Nothing                                                                  -- Error in processing the rest
+    Nothing ->  Nothing                                                                     -- Error in processing the current list
 sexprToAST (a:rest) = case sexprSListHandling [a] of
     Just resultA -> case sexprToAST rest of
         Just resultRest -> Just (resultA : resultRest)
         Nothing -> Nothing
     Nothing -> Nothing
 
-
--- sexprToAST _ = Just [ASTNumber 4] --debug
-
+    -- sexprToAST _ = Just [ASTNumber 4] --debug
 
 
 
-
-
-
-
--- sexprToAST :: SExpr -> Maybe AST
--- sexprToAST (SNumber n) = Just $ ASTNumber n
--- sexprToAST (SSymbol "#t") = Just $ ASTBoolean True
--- sexprToAST (SSymbol "#f") = Just $ ASTBoolean False
--- sexprToAST (SSymbol "define") = Nothing
--- sexprToAST (SSymbol x) = Just $ ASTSymbol x
--- sexprToAST (SList []) = Nothing
--- sexprToAST (SList [(SSymbol "define")]) = Nothing
--- sexprToAST (SList [(SSymbol "define"), (SSymbol x), expr]) = sexprToAST expr >>= (\expr -> Just $ ASTDefine x expr)
--- sexprToAST (SList ((SSymbol x) : xs))
---     | x == "define" = Nothing
---     | otherwise = mapM sexprToAST xs >>= (\xs -> Just $ ASTCall x xs)
