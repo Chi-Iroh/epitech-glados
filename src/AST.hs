533ccb6294aed3c44b0a6906f9acaaa0be9fc093
PERRIOLAT Nils <nils.perriolat@epitech.eu>
2024-12-02 06:23:52 -0500

    [ADD] deriving from Eq for SExpr and AST

--- src/AST.hs
+++ src/AST.hs
@@ -3,8 +3,8 @@ module AST
     AST(ASTDefine, ASTLambda, ASTSymbol, ASTNumber, ASTBoolean, ASTCall)
     ) where
 
-data Call = LambdaCall [AST] AST | FunctionCall String deriving Show
-data AST = ASTDefine String AST | ASTSymbol String | ASTNumber Int | ASTBoolean Bool | ASTCall Call [AST] | ASTLambda [AST] AST
+data Call = LambdaCall [AST] AST | FunctionCall String deriving (Eq, Show)
+data AST = ASTDefine String AST | ASTSymbol String | ASTNumber Int | ASTBoolean Bool | ASTCall Call [AST] | ASTLambda [AST] AST deriving Eq
 
 instance Show AST where
     show (ASTNumber n) = show n
