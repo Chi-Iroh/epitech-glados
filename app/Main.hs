147e6a7392c9603b1d0903dc6abac4cdbea2a3e0
Thomas Sayen <thomas.sayen@epitech.eu>
2024-12-09 23:46:21 +0100

    [FIX] Output of procedures

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,6 @@
 module Main (main) where
 
+import AST (MainAST, isProcedureType)
 import Converter
 import Evaluate (evaluateAST)
 import Parser
@@ -12,8 +13,14 @@ getFileName :: [String] -> Maybe String
 getFileName [a] = Just a
 getFileName _ = Nothing
 
-showAll :: Show a => [a] -> String
-showAll = unlines . filter (not . null) . map show
+showAll' :: Show a => [a] -> String
+showAll' = unlines . filter (not . null) . map show
+
+showAll :: [MainAST] -> String
+showAll [] = ""
+showAll args
+    | length args == 1 && isProcedureType (head args) = "#\\<procedure\\>"
+    | otherwise = showAll' args
 
 putResult :: Safe String -> IO ()
 putResult (Value res) = putStr res
