d39935f6269cb3313cac73b27579ba30cff5b8b3
Thomas Sayen <thomas.sayen@epitech.eu>
2024-12-09 15:37:27 +0100

    [UPDATE] Add Safe in AST evaluation

--- src/AST.hs
+++ src/AST.hs
@@ -1,13 +1,13 @@
-module AST
-    (Call(LambdaCall, FunctionCall),
-    AST(ASTDefine, ASTLambda, ASTSymbol, ASTNumber, ASTBoolean, ASTCall)
-    ) where
+module AST (Call(..), AST(..), MainAST(..)) where
 
 data Call = LambdaCall [AST] AST | FunctionCall String deriving (Eq, Show)
-data AST = ASTDefine String AST | ASTSymbol String | ASTNumber Int | ASTBoolean Bool | ASTCall Call [AST] | ASTLambda [AST] AST deriving Eq
 
-instance Show AST where
-    show (ASTNumber n) = show n
-    show (ASTBoolean b) = if b then "#t" else "#f"
-    show (ASTLambda _ _) = "#\\<procedure\\>"
+--
+data AST = ASTDefine String AST | ASTSymbol String | ASTNumber Int | ASTBoolean Bool | ASTCall Call [AST] | ASTLambda [AST] AST deriving (Eq, Show)
+data MainAST = MainAST AST
+
+instance Show MainAST where
+    show (MainAST (ASTNumber n)) = show n
+    show (MainAST (ASTBoolean b)) = if b then "#t" else "#f"
+    show (MainAST (ASTLambda _ _)) = "#\\<procedure\\>"
     show _ = ""
--- src/Evaluate.hs
+++ src/Evaluate.hs
@@ -1,31 +1,46 @@
-module Evaluate (evaluateAST, Symbol(..), Symbols, symbolName) where
+module Evaluate (evaluateAST, Symbol(..), Symbols) where
 
-import AST (AST(..), Call(..))
+import AST (AST(..), Call(..), MainAST(..))
+import Debug (debug2)
 import Data.List (singleton)
-
-builtins :: Symbols
-builtins = [BackendSymbol ("*", astArithmeticOp (*))
-            , BackendSymbol ("+", astArithmeticOp (+))
-            , BackendSymbol ("-", astArithmeticOp (-))
-            , BackendSymbol ("div", astArithmeticOp div)
-            , BackendSymbol ("mod", astArithmeticOp mod)
-            , BackendSymbol (">", astComparisonOp (>))
-            , BackendSymbol (">=", astComparisonOp (>=))
-            , BackendSymbol ("<", astComparisonOp (<))
-            , BackendSymbol ("<=", astComparisonOp (<=))
-            , BackendSymbol ("eq?", astComparisonOp (==))
-            , BackendSymbol ("==", astComparisonOp (==))
-            , BackendSymbol ("if", astIf)]
+import Utils (Safe(..))
 
 find :: (a -> Bool) -> [a] -> Maybe a
 find _ [] = Nothing
 find f (x : xs) = if (f x) then Just x else find f xs
 
-data Symbol = BackendSymbol (String, (Symbols -> [AST] -> Maybe AST))
+data Symbol = BackendSymbol (String, (Symbols -> [AST] -> Safe AST))
 type Symbols = [Symbol]
 
 symbolName :: Symbol -> String
 symbolName (BackendSymbol (s, _)) = s
+
+showSymbols :: Symbols -> String
+showSymbols symbols = show (map symbolName symbols)
+
+traceSymbols2 :: String -> Symbols -> Symbols
+traceSymbols2 msg f = debug2 msg (map symbolName f) f
+
+traceSymbols :: Symbols -> Symbols
+traceSymbols = traceSymbols2 "symbols: "
+
+traceSymbol :: Maybe Symbol -> Maybe Symbol
+traceSymbol f@(Just (BackendSymbol (s, _))) = debug2 "symbol: " s f
+traceSymbol Nothing = Nothing
+
+builtins :: Symbols
+builtins = [    BackendSymbol ("*", astArithmeticOp "*" (*))
+            ,   BackendSymbol ("+", astArithmeticOp "+" (+))
+            ,   BackendSymbol ("-", astArithmeticOp "-" (-))
+            ,   BackendSymbol ("div", astArithmeticOp "div" div)
+            ,   BackendSymbol ("mod", astArithmeticOp "mod" mod)
+            ,   BackendSymbol (">", astComparisonOp ">" (>))
+            ,   BackendSymbol (">=", astComparisonOp ">=" (>=))
+            ,   BackendSymbol ("<", astComparisonOp "<" (<))
+            ,   BackendSymbol ("<=", astComparisonOp "<=" (<=))
+            ,   BackendSymbol ("eq?", astComparisonOp "eq?" (==))
+            ,   BackendSymbol ("==", astComparisonOp "==" (==))
+            ,   BackendSymbol ("if", astIf)]
 
 updateOrAdd :: (a -> Bool) -> a -> [a] -> [a]
 updateOrAdd _ a [] = [a]
@@ -33,77 +48,86 @@ updateOrAdd f a (x : xs)
     | f x = a : xs
     | otherwise = x : updateOrAdd f a xs
 
-registerSymbol :: Symbols -> String -> (Symbols -> [AST] -> Maybe AST) -> Symbols
+registerSymbol :: Symbols -> String -> (Symbols -> [AST] -> Safe AST) -> Symbols
 registerSymbol symbols name f = updateOrAdd ((== name) . symbolName) (BackendSymbol (name, f)) symbols
 
-symbolId :: AST -> Symbols -> [AST] -> Maybe AST
-symbolId value _ [] = Just value
-symbolId _ _ _ = Nothing
+symbolId :: String -> AST -> Symbols -> [AST] -> Safe AST
+symbolId _ value _ [] = Value value
+symbolId name _ _ args = Error ("Value " ++ name ++ " must be called without any argument, but you provided " ++ show args ++ " !")
 
-fromSymbol :: AST -> Maybe String
-fromSymbol (ASTSymbol s) = Just s
-fromSymbol _ = Nothing
+fromSymbol :: AST -> Safe String
+fromSymbol (ASTSymbol s) = Value s
+fromSymbol arg = Error ("Lambda arguments list must only contain symbols, but got " ++ show arg ++ " !")
 
 updateSymbols :: Symbols -> [(String, AST)] -> Symbols
-updateSymbols symbols new = foldr (\(symbol, value) symbols' -> registerSymbol symbols' symbol (symbolId value)) symbols new
+updateSymbols symbols new = foldr (\(symbol, value) symbols' -> registerSymbol symbols' symbol (symbolId symbol value)) symbols new
 
-makeLambdaSymbols :: Symbols -> [AST] -> [AST] -> Maybe Symbols
-makeLambdaSymbols symbols [] _ = Just symbols
-makeLambdaSymbols symbols _ [] = Just symbols
+makeLambdaSymbols :: Symbols -> [AST] -> [AST] -> Safe Symbols
+makeLambdaSymbols symbols [] _ = Value symbols
+makeLambdaSymbols symbols _ [] = Value symbols
 makeLambdaSymbols symbols lambdaParams lambdaArgs = fmap (\params' -> updateSymbols symbols (zip params' lambdaArgs)) (mapM fromSymbol lambdaParams)
 
-evaluateAST1 :: Symbols -> AST -> (Maybe AST, Symbols)
-evaluateAST1 symbols n@(ASTNumber _) = (Just n, symbols)
-evaluateAST1 symbols b@(ASTBoolean _) = (Just b, symbols)
-evaluateAST1 symbols lambda@(ASTLambda _ _) = (Just lambda, symbols)
-evaluateAST1 symbols (ASTSymbol s) = (find ((== s) . symbolName) symbols >>= (\(BackendSymbol (_, f)) -> f symbols [] >>= (fst . evaluateAST1 symbols)), symbols)
-evaluateAST1 symbols (ASTCall (FunctionCall f) args) = (find ((== f) . symbolName) symbols >>= (\(BackendSymbol (_, f')) -> f' symbols args), symbols)
+toSafe :: String -> Maybe a -> Safe a
+toSafe err Nothing = Error err
+toSafe _ (Just a) = Value a
+
+findSymbol :: Symbols -> String -> Safe Symbol
+findSymbol symbols symbol = toSafe ("Symbol " ++ symbol ++ " not found ! Existing symbols: " ++ showSymbols symbols) (find ((== symbol) . symbolName) symbols)
+
+evaluateAST1 :: Symbols -> AST -> (Safe AST, Symbols)
+evaluateAST1 symbols n@(ASTNumber _) = (Value n, symbols)
+evaluateAST1 symbols b@(ASTBoolean _) = (Value b, symbols)
+evaluateAST1 symbols lambda@(ASTLambda _ _) = (Value lambda, symbols)
+evaluateAST1 symbols (ASTSymbol s) = (findSymbol symbols s >>= (\(BackendSymbol (_, f)) -> f symbols [] >>= (fst . evaluateAST1 symbols)), symbols)
+evaluateAST1 symbols (ASTCall (FunctionCall f) args) = (findSymbol symbols f >>= (\(BackendSymbol (_, f')) -> f' symbols args), symbols)
 evaluateAST1 symbols (ASTCall (LambdaCall params body) args) = (args' >>= (makeLambdaSymbols symbols params) >>= (\symbols' -> fst $ evaluateAST1 symbols' body), symbols) -- discarding lambda symbols and returning old ones
-    where args' = if null args then Just [] else (mapM (fst . evaluateAST1 symbols) args)
-evaluateAST1 symbols define@(ASTDefine s (ASTLambda params body)) = (Just define, registerSymbol symbols s function)
+    where args' = if null args then Value [] else (mapM (fst . evaluateAST1 symbols) args)
+evaluateAST1 symbols define@(ASTDefine s (ASTLambda params body)) = (Value define, registerSymbol symbols s function)
     where function symbols' args = mapM (fst . evaluateAST1 symbols') args >>= (\args' -> fst $ evaluateAST1 symbols' (ASTCall (LambdaCall params body) args'))
-evaluateAST1 symbols define@(ASTDefine s ast) = (Just define, registerSymbol symbols s function)
+evaluateAST1 symbols define@(ASTDefine s ast) = (Value define, registerSymbol symbols s function)
     where function symbols' args
-            | null args = Just ast
-            | length args >= 2 = Nothing
+            | null args = Value ast
+            | length args >= 2 = Error ("Too many arguments when calling symbol ! Got " ++ show (length args) ++ " but expected 0 or 1 !")
             | otherwise = fst $ evaluateAST1 symbols' $ head args
 
-astArithmeticOp' :: (Int -> Int -> Int) -> [AST] -> Maybe AST
-astArithmeticOp' f [(ASTNumber a), (ASTNumber b)] = Just $ ASTNumber (f a b)
-astArithmeticOp' _ _ = Nothing
+astArithmeticOp' :: String -> (Int -> Int -> Int) -> [AST] -> Safe AST
+astArithmeticOp' _ f [(ASTNumber a), (ASTNumber b)] = Value $ ASTNumber (f a b)
+astArithmeticOp' name _ args = Error ("Bad arguments when attempting to call " ++ name ++ " ! Expected 2 integers but got " ++ show args ++ " !")
 
-astArithmeticOp :: (Int -> Int -> Int) -> Symbols -> [AST] -> Maybe AST
-astArithmeticOp f symbols args = mapM (fst . evaluateAST1 symbols) args >>= astArithmeticOp' f
+astArithmeticOp :: String -> (Int -> Int -> Int) -> Symbols -> [AST] -> Safe AST
+astArithmeticOp name f symbols args = mapM (fst . evaluateAST1 symbols) args >>= astArithmeticOp' name f
 
-toNumber :: AST -> Maybe Int
-toNumber (ASTBoolean b) = Just (fromEnum b)
-toNumber (ASTNumber n) = Just n
-toNumber _ = Nothing
+toNumber :: AST -> Safe Int
+toNumber (ASTBoolean b) = Value (fromEnum b)
+toNumber (ASTNumber n) = Value n
+toNumber a = Error ("Cannot convert " ++ show a ++ " to an integer !")
 
-astComparisonOp' :: (Int -> Int -> Bool) -> [AST] -> Maybe AST
-astComparisonOp' f args@[_, _] = mapM toNumber args >>= compare'
-    where compare' [a', b'] = Just $ ASTBoolean (f a' b')
-          compare' _ = Nothing
-astComparisonOp' _ _ = Nothing
+astComparisonOp' :: String -> (Int -> Int -> Bool) -> [AST] -> Safe AST
+astComparisonOp' name f args@[_, _] = mapM toNumber args >>= compare'
+    where compare' [a', b'] = Value $ ASTBoolean (f a' b')
+          compare' args = Error ("Bad arguments when attempting to call " ++ name ++ ", can only compare booleans and integers, but got " ++ show args ++ " !")
+astComparisonOp' name _ args = Error ("Bad arguments when attempting to call " ++ name ++ ", can only compare 2 arguments, but got " ++ show (length args) ++ " !")
 
-astComparisonOp :: (Int -> Int -> Bool) -> Symbols -> [AST] -> Maybe AST
-astComparisonOp f symbols args = mapM (fst . evaluateAST1 symbols) args >>= astComparisonOp' f
+astComparisonOp :: String -> (Int -> Int -> Bool) -> Symbols -> [AST] -> Safe AST
+astComparisonOp name f symbols args = mapM (fst . evaluateAST1 symbols) args >>= astComparisonOp' name f
 
-astIf' :: Symbols -> [AST] -> Maybe AST
-astIf' symbols [(ASTBoolean condition), a, b] = if condition then fst (evaluateAST1 symbols a) else fst (evaluateAST1 symbols b)
-astIf' _ _ = Nothing
+astIf' :: Symbols -> [AST] -> Safe AST
+astIf' symbols [(ASTBoolean condition), a, b] = if condition then eval a else eval b
+    where eval s = fst (evaluateAST1 symbols s)
+astIf' _ args = Error ("if must be called as 'if <condition as boolean> <a> <b>', but got args " ++ show args)
 
-astIf :: Symbols -> [AST] -> Maybe AST
+astIf :: Symbols -> [AST] -> Safe AST
 astIf symbols [a, b, c] = (fst $ evaluateAST1 symbols a) >>= (\a' -> astIf' symbols [a', b, c])
-astIf _ _ = Nothing
+astIf _ args = Error ("if must be called with 3 arguments, but got " ++ show (length args))
 
-evaluateAST' :: Symbols -> [AST] -> Maybe [AST]
-evaluateAST' _ [] = Nothing
-evaluateAST' f [x] = fmap singleton (fst $ evaluateAST1 f x)
-evaluateAST' f (x : xs) = evaluated >>= (\x' -> evaluateAST' symbols xs >>= (\xs' -> Just (x' : xs')))
+evaluateAST' :: Symbols -> [AST] -> Safe [MainAST]
+evaluateAST' _ [] = Error "Nothing to evaluate !"
+evaluateAST' f [x] = fmap (singleton . MainAST) (fst $ evaluateAST1 f x)
+evaluateAST' f (x : xs) = liftA2 (:) (fmap MainAST evaluated) rest
     where (evaluated, symbols) = evaluateAST1 f x
+          rest = evaluateAST' symbols xs
 
-evaluateAST :: [AST] -> Maybe [AST]
+evaluateAST :: [AST] -> Safe [MainAST]
 evaluateAST = evaluateAST' builtins
 
 -- test :: [SExpr]
@@ -111,7 +135,7 @@ evaluateAST = evaluateAST' builtins
         -- , List [Symbol "eq?", Number 1, List [Symbol "mod", List [Symbol "div", List [Symbol "*", Number 5, List [Symbol "+", Number 7, List [Symbol "-", Number 10, Number 2]]], Number 5], Number 7]]
         -- , List [Symbol "if", List [Symbol ">", Symbol "x", Number 8], Symbol "#t", Symbol "#f"] ]
 -- test = [ List [Symbol "define", Symbol "a", Number 5], List [List [Lambda (List [Symbol "a", Symbol "b"]) (List [Symbol "+", Symbol "a", Symbol "b"])], List [Symbol "+", Symbol "a", Symbol "a"], Symbol "a"]]
--- test = [List [Symbol "define", Symbol "x", List [Lambda (List [Symbol "a", Symbol "b"]) (List [Symbol "+", Symbol "a", Symbol "b"])]], List [Symbol "x", Number 5, Number 10]]
+-- test = [List [Symbol "define", Symbol "x", Li /home/Chi_Iroh/.gst [Lambda (List [Symbol "a", Symbol "b"]) (List [Symbol "+", Symbol "a", Symbol "b"])]], List [Symbol "x", Number 5, Number 10]]
 -- test = [List [List [Lambda (List []) (Number 15)]]]
 -- test = [List [List [Lambda (List [Symbol "a"]) (List [Symbol "if", List [Symbol "eq?", Symbol "a", Number 1], Symbol "#t", Symbol "#f"])], Number 1]]
 -- test = [ List [Symbol "define", Symbol "factorial", List [Lambda (List [Symbol "n"]) (List [Symbol "if", List [Symbol "<=", Symbol "n", Number 1], Number 1, List [Symbol "*", Symbol "n", List [Symbol "factorial", List [Symbol "-", Symbol "n", Number 1]]]])]], List [Symbol "factorial", Number 10]]
