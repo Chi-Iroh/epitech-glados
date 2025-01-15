2ec27604ba21809338a2b6409611173c26a91ab6
Marion K <marion.kauffmann@epitech.eu>
2025-01-15 16:33:00 +0100

    [GIT] commit my changes

--- src/Deserialize.hs
+++ src/Deserialize.hs
@@ -31,6 +31,9 @@ instance Deserializeble Char where
 instance Deserializable Int where
     deserialize i = deserializeInt i
 
+instance Deserializable Float where
+    deserialize f = deserializeInt f
+
 instance DeserializableType Null where
     deserialize _ = deserializeTypeNull
 
@@ -41,12 +44,55 @@ deserializeBool [0x01] = True
 deserializeChar :: [Word8] -> Char
 deserializeChar [w] = chr (fromIntegral w)
 
+deserializeUInt :: [Word8] -> Int
+deserializeUInt uint
+    | not (checkUInt (deserializeInt' uint)) = error "Negative uint !"
+    | otherwise = deserializeInt' uint
+
 deserializeInt :: [Word8] -> Int
-deserializeInt [b1, b2, b3, b4] =
+deserializeInt int
+    | not (checkInt (deserializeInt' int)) = error "Out of range int !"
+    | otherwise = deserializeInt' int
+
+deserializeInt' :: [Word8] -> Int
+deserializeInt' [b1, b2, b3, b4] =
     (fromIntegral w1 `shiftL` 24) .|.
     (fromIntegral w2 `shiftL` 16) .|.
     (fromIntegral w3 `shiftL` 8)  .|.
     fromIntegral w4
+deserializeInt' _ = error "Not a number"
+
+deserializeFloat :: [Word8] -> Float
+deserializeFloat float
+    | not (checkFloat (deserializeFloat' float)) = error "Out of range float !"
+    | otherwise = deserializeFloat' float
+
+deserializeFloat' :: [Word8] -> Float
+deserializeFloat' [w1, w2, w3, w4] =
+    wordToFloat $
+    (fromIntegral w1 `shiftL` 24) .|.
+    (fromIntegral w2 `shiftL` 16) .|.
+    (fromIntegral w3 `shiftL` 8)  .|.
+    fromIntegral w4
+
+combineWord32 :: [Word8] -> Word32
+combineWord32 [w1, w2, w3, w4] =
+    (fromIntegral w1 `shiftL` 24) .|.
+    (fromIntegral w2 `shiftL` 16) .|.
+    (fromIntegral w3 `shiftL` 8)  .|.
+    fromIntegral w4
+combineWord32 _ = error "Invalid input for combining Word32"
+
+deserializeInt :: [Word8] -> Int
+deserializeInt bytes
+    | all (== 0xFF) bytes = -1
+    | testBit combined 31 =
+        let absValue = complement (fromEnum (clearSignBit combined)) + 1
+        in -absValue
+    | otherwise = fromEnum combined
+  where
+    combined = combineWord32 bytes
+    clearSignBit w = w `clearBit` 31
 
 
 
