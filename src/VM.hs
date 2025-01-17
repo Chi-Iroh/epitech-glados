46f8b223ec10196b07452a74d8d186a7990abda5
Maksen <maksen.mofredj@gmail.com>
2025-01-17 13:03:06 +0100

    [ADD] list deserialization

--- src/VM.hs
+++ src/VM.hs
@@ -7,7 +7,6 @@ import System.Exit (die)
 import AssemblyInstructions (AssemblyInstruction(..))
 import BinaryIO (readBinary, writeBinary)
 import Bits (splitWord32)
-import ByteCode
 import Deserialize (deserialize, deserializeType)
 import SymbolTable (readSymbolTable, SymbolTable)
 import Type (Type(..))
@@ -19,17 +18,17 @@ fromSafe (Error err) = error err
 fromSafe (Value a) = a
 
 pushRegister :: Int -> [Maybe Any] -> [Any] -> Safe [Any]
-pushRegister 0 ((Just a):as) stack = Value $ a:stack
-pushRegister 0 (Nothing:as) _ = Error "Register empty"
-pushRegister x (a:as) stack = pushRegister (x - 1) as stack
+pushRegister 0 ((Just a):_) stack = Value $ a:stack
+pushRegister 0 (Nothing:_) _ = Error "Register empty"
+pushRegister x (_:as) stack = pushRegister (x - 1) as stack
 pushRegister _ [] _ = Error "Register out of bounds"
 
 pushValue :: Any -> [Any] -> Safe [Any]
 pushValue a stack = Value $ a:stack
 
 popStack :: Int -> [Any] -> [Maybe Any] -> Safe [Maybe Any]
-popStack 0 (a:as) (_:rs) = Value $ Just a : rs
-popStack x stack (r:rs) = popStack (x - 1) stack rs
+popStack 0 (a:_) (_:rs) = Value $ Just a : rs
+popStack x stack (_:rs) = popStack (x - 1) stack rs
 popStack _ [] _ = Error "Stack empty"
 popStack _ _ [] = Error "Register out of bounds"
 
@@ -39,25 +38,29 @@ endOfFile = "End of file"
 mapFst :: (a -> c) -> (a, b) -> (c, b)
 mapFst f (a, b) = (f a, b)
 
+deserializeList :: Type -> Int -> [Word8] -> [Any] -> Safe ([Any], Int)
+deserializeList _ len 0x00 list = Value (list, len)
+deserializeList _ _ [] _ = Error "No end bytes in list"
+deserializeList a len bytes list = deserialize a bytes >>= (\(member, len', rest) -> deserializeList a (len + len') rest $ member : list)
+
 deserializeTypeAndValue :: [Word8] -> Safe (Any, Int)
 deserializeTypeAndValue bytes = deserializeType bytes >>= (\(_type, len, rest) -> deserialize _type rest >>= \(a, len', rest') -> Value (Any (_type, a), len + len'))
 
 parseInstruction :: [Word8] -> Safe (AssemblyInstruction, Int)
 parseInstruction (pushVal : xs) = mapFst PushValue <$> deserializeTypeAndValue xs
-
--- parseInstruction (pushReg : reg : xs) = Safe (pushRegister reg)
--- parseInstruction (pop : xs) = snd (fromSafe $ deserialize xs)
--- parseInstruction (construct : xs) = snd (fromSafe $ deserialize xs)
--- parseInstruction (test : xs) = snd (fromSafe $ deserialize xs)
+parseInstruction (pushReg : reg : xs) = Value (PushRegister reg, 2)
+parseInstruction (pop : reg : xs) = Value (Pop reg, 2)
+-- parseInstruction (construct : xs) = mapFst Construct <$> deserializeTypeAndValue xs >>= deserializeTypeAndValue xs
+parseInstruction (test : reg : xs) = Value (Test reg, 2)
 -- parseInstruction (jt : xs) = snd (fromSafe $ deserialize xs)
 -- parseInstruction (jf : xs) = snd (fromSafe $ deserialize xs)
--- parseInstruction (call : xs) = snd (fromSafe $ deserialize xs)
--- parseInstruction (retVal : xs) = snd (fromSafe $ deserialize xs)
--- parseInstruction (retReg : xs) = snd (fromSafe $ deserialize xs)
--- parseInstruction (movVal : xs) = snd (fromSafe $ deserialize xs)
--- parseInstruction (movReg : xs) = snd (fromSafe $ deserialize xs)
--- parseInstruction (outVal : xs) = snd (fromSafe $ deserialize xs)
--- parseInstruction (outReg : xs) = snd (fromSafe $ deserialize xs)
+-- parseInstruction (call : xs) = mapFst Call <$> deserializeList T_String 0 xs [] 
+parseInstruction (retVal : xs) = mapFst RetValue <$> deserializeTypeAndValue xs
+parseInstruction (retReg : reg : xs) = Value (RetRegister reg, 2)
+parseInstruction (movVal : reg : xs) = mapFst (MovValue reg) <$> deserializeTypeAndValue xs
+parseInstruction (movReg : reg1 : reg2 : xs) = Value (MovRegister ref1 reg2, 3)
+parseInstruction (outVal : xs) = mapFst OutValue <$> deserializeTypeAndValue xs
+parseInstruction (outReg : reg : xs) = Value (RetRegister reg, 2)
 parseInstruction [] = Error endOfFile -- maybe special handling to return 0 instead of 1 when  end of file, because it's totally normal behavior
 
 mainVM :: FilePath -> IO ()
