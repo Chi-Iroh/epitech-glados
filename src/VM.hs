032121636e4fe5455239c0b6c9ad7426a48b50ea
Maksen <maksen.mofredj@gmail.com>
2025-01-17 22:33:34 +0100

    [ADD] created new parseInstruction func to handle old one && named old one parseInstruction' ++ created base of executeInstruction && fix pushReg

--- src/VM.hs
+++ src/VM.hs
@@ -6,9 +6,9 @@ import System.Exit (die)
 import Text.Printf (printf)
 import Unsafe.Coerce (unsafeCoerce)
 
-import AssemblyInstructions (AssemblyInstruction(..))
+import AssemblyInstructions (AssemblyInstruction(..), RegisterID)
 import BinaryIO (readBinary, writeBinary)
-import Bits (splitWord32, combineWord32)
+import Bits (splitWord32, combineWord32, u32)
 import Deserialize (deserialize, deserializeType, deserializeInt)
 import SymbolTable (readSymbolTable, SymbolTable)
 import Serialize (Serializable, serialize)
@@ -20,10 +20,10 @@ fromSafe :: Safe a -> a
 fromSafe (Error err) = error err
 fromSafe (Value a) = a
 
-pushRegister :: Int -> [Maybe Any] -> [Any] -> Safe [Any]
-pushRegister 0 ((Just a):_) stack = Value $ a:stack
+pushRegister :: RegisterID -> [Maybe Any] -> [Any] -> Safe ([Maybe Any], [Any])
+pushRegister 0 ((Just a):reg) stack = Value (Nothing:reg, a:stack)
 pushRegister 0 (Nothing:_) _ = Error "Register empty"
-pushRegister x (_:as) stack = pushRegister (x - 1) as stack
+pushRegister x (a:as) stack = pushRegister (x - 1) as stack >>=(\(_reg, _stack) -> Value (a:_reg, _stack))
 pushRegister _ [] _ = Error "Register out of bounds"
 
 pushValue :: Any -> [Any] -> Safe [Any]
@@ -49,37 +49,41 @@ deserializeList a len bytes list = deserialize a bytes >>= (\(member, len', rest
 deserializeTypeAndValue :: [Word8] -> Safe (Any, Int)
 deserializeTypeAndValue bytes = deserializeType bytes >>= (\(_type, len, rest) -> deserialize _type rest >>= \(a, len', rest') -> Value (Any (_type, a), len + len'))
 
-parseInstruction :: [Word8] -> Safe (AssemblyInstruction, Int)
-parseInstruction (0x00 : xs) = mapFst PushValue <$> deserializeTypeAndValue xs
-parseInstruction (0x01 : reg : xs) = Value (PushRegister reg, 2)
-parseInstruction (0x10 : reg : xs) = Value (Pop reg, 2)
-parseInstruction (0x20 : xs) = deserializeType xs >>=(\(_type, len, rest) -> deserializeInt xs >>= \(a, len', _) -> Value (Construct _type a, len + len'))
-parseInstruction (0x30 : reg : xs) = Value (Test reg, 2)
-parseInstruction (0x40 : byte1 : byte2 : byte3 : byte4 : xs) = Value (JumpIfTrue (combineWord32 $ byte1 : [byte2] ++ [byte3] ++ [byte4]), 5)
-parseInstruction (0x50 : byte1 : byte2 : byte3 : byte4 : xs) = Value (JumpIfFalse (combineWord32 $ byte1 : [byte2] ++ [byte3] ++ [byte4]), 5)
-parseInstruction (0x60 : xs) = deserializeList T_Char 0 xs [] <&> (\(str, i) -> (map (\(Any (_, a)) -> unsafeCoerce a :: Char) str, i)) <&> mapFst Call
-parseInstruction (0x70 : xs) = mapFst RetValue <$> deserializeTypeAndValue xs
-parseInstruction (0x71 : reg : xs) = Value (RetRegister reg, 2)
-parseInstruction (0x80 : reg : xs) = mapFst (MovValue reg) <$> deserializeTypeAndValue xs
-parseInstruction (0x81 : reg1 : reg2 : xs) = Value (MovRegister reg1 reg2, 3)
-parseInstruction (0x90 : xs) = mapFst OutValue <$> deserializeTypeAndValue xs
-parseInstruction (0x91 : reg : xs) = Value (RetRegister reg, 2)
-parseInstruction (_ : xs) = parseInstruction xs
-parseInstruction [] = Error endOfFile -- maybe special handling to return 0 instead of 1 when  end of file, because it's totally normal behavior
+executeInstruction :: AssemblyInstruction -> Vm -> Safe Vm
+executeInstruction (PushRegister registerID) (Vm reg cstack bf vstack pc) = pushRegister registerID reg vstack >>=(\(_reg, _stack) -> Value (Vm _reg cstack bf vstack pc))
+
+parseInstruction' :: [Word8] -> Safe (AssemblyInstruction, Int)
+parseInstruction' (0x00 : xs) = mapFst PushValue <$> deserializeTypeAndValue xs
+parseInstruction' (0x01 : reg : xs) = Value (PushRegister reg, 2)
+parseInstruction' (0x10 : reg : xs) = Value (Pop reg, 2)
+parseInstruction' (0x20 : xs) = deserializeType xs >>=(\(_type, len, rest) -> deserializeInt xs >>= \(a, len', _) -> Value (Construct _type a, len + len'))
+parseInstruction' (0x30 : reg : xs) = Value (Test reg, 2)
+parseInstruction' (0x40 : byte1 : byte2 : byte3 : byte4 : xs) = Value (JumpIfTrue (combineWord32 $ byte1 : [byte2] ++ [byte3] ++ [byte4]), 5)
+parseInstruction' (0x50 : byte1 : byte2 : byte3 : byte4 : xs) = Value (JumpIfFalse (combineWord32 $ byte1 : [byte2] ++ [byte3] ++ [byte4]), 5)
+parseInstruction' (0x60 : xs) = deserializeList T_Char 0 xs [] <&> (\(str, i) -> (map (\(Any (_, a)) -> unsafeCoerce a :: Char) str, i)) <&> mapFst Call
+parseInstruction' (0x70 : xs) = mapFst RetValue <$> deserializeTypeAndValue xs
+parseInstruction' (0x71 : reg : xs) = Value (RetRegister reg, 2)
+parseInstruction' (0x80 : reg : xs) = mapFst (MovValue reg) <$> deserializeTypeAndValue xs
+parseInstruction' (0x81 : reg1 : reg2 : xs) = Value (MovRegister reg1 reg2, 3)
+parseInstruction' (0x90 : xs) = mapFst OutValue <$> deserializeTypeAndValue xs
+parseInstruction' (0x91 : reg : xs) = Value (RetRegister reg, 2)
+parseInstruction' (_ : xs) = parseInstruction' xs
+parseInstruction' [] = Error endOfFile -- maybe special handling to return 0 instead of 1 when  end of file, because it's totally normal behavior
+
+parseInstruction :: Vm -> [Word8] -> Safe Vm
+parseInstruction vm bytes = parseInstruction' bytes >>=(\(instruction, movement) -> case executeInstruction instruction vm of
+    Error err -> Error err
+    Value (Vm _reg _cstack _bf _vstack _pc) -> Value (Vm _reg _cstack _bf _vstack (_pc + u32 movement))) 
+
+-- Missing parseFile command between parse instruction && mainVM
 
 mainVM :: FilePath -> IO ()
 mainVM path = do
     file <- readBinary path
     case readSymbolTable file of
         Error err -> die err
-        Value (table, rest) -> case parseInstruction rest of
+        Value (table, rest) -> case parseInstruction defaultVM rest of
             Error err' -> die err'
-            Value v -> print v
+            Value (Vm _reg _cstack _bf _vstack _pc) -> print _pc
             -- writeBinary "output.txt"
 
--- to do : 
--- - sortie de parseByte = fst -> associated call && snd -> parseByte snd
--- - init VM data
--- - pass it through 
--- scope changing cond : call
--- deserialize xs >>= 
