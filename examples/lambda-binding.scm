56ba908ff9fe35449826006c5b1f07c669dab8f8
PERRIOLAT Nils <nils.perriolat@epitech.eu>
2025-01-22 07:37:04 -0500

    [FIX] type synthaxe in func test

--- examples/lambda-binding.scm
+++ examples/lambda-binding.scm
@@ -1,2 +1,2 @@
-(define add <(number number) => number> (lambda (a::number b::number) (+ a b) number))
+(define add <-(number number) => number-> (lambda (a::number b::number) (+ a b) number))
 (add 3 4)
